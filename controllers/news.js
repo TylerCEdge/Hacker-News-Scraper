8cf74871615a3f6459b7edf636754de351734537
atkmstr <atkmstr@localhost.localdomain>
2019-05-24 19:00:40 -0400

    managed to get scraper working on a page of my choosing.  Scraper now removes unwanted characters from titles and prevents article duplicates.  Trying to figure out how to get more than one page of articles

--- controllers/news.js
+++ controllers/news.js
@@ -6,13 +6,13 @@ var db = require("../models")
 
 
 // Default route renders the index handlebars view
-router.get('/', function(req, res){
-	res.render('index');
+router.get('/', function (req, res) {
+    res.render('index');
 });
 
-router.get('/test', function(req, res){
-	res.render('test');
-	ScrapeArticles();
+router.get('/test', function (req, res) {
+    ScrapeArticles();
+    res.render('test');
 });
 
 
@@ -21,38 +21,96 @@ router.get('/test', function(req, res){
 // A function for scraping the echoJS website
 function ScrapeArticles() {
     // First, we grab the body of the html with axios
-    axios.get("http://www.echojs.com/").then(function (response) {
+    axios.get("https://thehackernews.com/").then(function (response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
+        // console.log(response.data)
 
         // Now, we grab every h2 within an article tag, and do the following:
-        $("article h2").each(function (i, element) {
+        $("h2").each(function () {
             // Save an empty result object
             var result = {};
 
             // Add the text and href of every link, and save them as properties of the result object
             result.title = $(this)
-                .children("a")
-                .text();
+                .text()
+                // Removes unwanted characters from the titles
+                .replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, ' ');
             result.link = $(this)
-                .children("a")
+                .parent().parent().parent().parent()
+                .find("a")
                 .attr("href");
 
-            // Create a new Article using the `result` object built from scraping
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    // View the added result in the console
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-                    // If an error occurred, log it
+            // Prevents Article replication by checking if it exists already
+            db.Article.findOne({ 'title': result.title }, function (err, res) {
+                if (err) {
                     console.log(err);
-                });
+                }
+                else {
+                    if (res == null) {
+                        // Create a new Article using the `result` object built from scraping
+                        db.Article.create(result)
+                            .then(function (dbArticle) {
+                                // View the added result in the console
+                                console.log(dbArticle);
+                            })
+                            .catch(function (err) {
+                                // If an error occurred, log it
+                                console.log(err);
+                            });
+                    }
+                }
+            });
         });
-
-        // Send a message to the client
-        res.send("Scrape Complete");
-	});
+    });
 };
+
+// Route for getting all Articles from the db
+router.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+    // TODO: Finish the route so it grabs all of the articles
+  });
+  
+  // Route for grabbing a specific Article by id, populate it with it's note
+  router.get("/articles/:id", function (req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+    // TODO
+    // ====
+    // Finish the route so it finds one article using the req.params.id,
+    // and run the populate method with "note",
+    // then responds with the article with the note included
+  });
+  
+  // Route for saving/updating an Article's associated Note
+  router.post("/articles/:id", function (req, res) {
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+    // TODO
+    // ====
+    // save the new note that gets posted to the Notes collection
+    // then find an article from the req.params.id
+    // and update it's "note" property with the _id of the new note
+  });
 
 module.exports = router;
