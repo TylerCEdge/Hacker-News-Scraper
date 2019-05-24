00e2b6cbddb2f5666c868fc46db38c866d275113
atkmstr <atkmstr@localhost.localdomain>
2019-05-24 16:38:32 -0400

    start scraping button is linked to test and /scrape route for getting articles has been modified into a function that is called once test route is triggered

--- public/app.js
+++ public/app.js
@@ -1,11 +1,11 @@
-// // Grab the articles as a json
-// $.getJSON("/articles", function(data) {
-//     // For each one
-//     for (var i = 0; i < data.length; i++) {
-//       // Display the apropos information on the page
-//       $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-//     }
-//   });
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+  });
 
   // Whenever someone clicks a p tag
   $(document).on("click", "p", function() {
